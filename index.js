093e212fd39d4a91d870fcd2521aaab059ca6fe5
phinx <phinx@foxmail.com>
2021-09-05 18:38:14 +0800

    test

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const tools = require('./lib');
 const { validator, transactions, cryptography } = require('@liskhq/lisk-client');
 const testkey= require('./secret/keys.json');
 const { publicKey, address } = cryptography.getAddressAndPublicKeyFromPassphrase(testkey.passphrase1);
-console.log(address);
+tools.getClient().then(async client => {
+    const account = await client.account.get(address);
+    console.log(account);
+});
